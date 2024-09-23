2e47ac68200186871a12008d8221882c43827989
Faizan Afzal <35985070+faizanafzal54@users.noreply.github.com>
2024-09-24 00:44:45 +0500

    Update config.js

--- client/src/configs/config.js
+++ client/src/configs/config.js
@@ -2,7 +2,7 @@ module.exports =
   process.env.NODE_ENV === "production"
     ? {
         apiUrl: "http://localhost:5000/api/",
-        facebookAppId: "",
+        facebookAppId: "", // your facebook app id
       }
     : {
         apiUrl: "http://localhost:5000/api/",
