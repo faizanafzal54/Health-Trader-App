39fdbf0011758e6276409e4a7e08f27ca50a4bb5
Faizan Afzal <35985070+faizanafzal54@users.noreply.github.com>
2024-09-24 00:42:23 +0500

    Update transporter.js

--- utils/transporter.js
+++ utils/transporter.js
@@ -2,11 +2,11 @@ let nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   // service: "gmail",
-  host: "smtp.office365.com",
+  host: "smtp.office365.com", // your smtp host
   port: 587,
   auth: {
-    user: "",
-    pass: ``,
+    user: "", // smtp user
+    pass: ``, // smtp password
   },
 });
 
